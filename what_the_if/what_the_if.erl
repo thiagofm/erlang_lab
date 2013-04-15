406b666b12c36414d7ed394282bc87955b00ed96
Thiago Fernandes Massa <thiagown@gmail.com>
2013-04-15 20:42:03 -0300

    elsif

--- what_the_if/what_the_if.erl
+++ what_the_if/what_the_if.erl
@@ -1,5 +1,5 @@
 -module(what_the_if).
--export([heh_fine/0]).
+-export([heh_fine/0, help_me/1]).
  
 heh_fine() ->
   if 1 =:= 1 ->
@@ -11,3 +11,12 @@ heh_fine() ->
   if 1 =:= 2, 1 =:= 1 ->
     fails
   end.
+
+help_me(Animal) ->
+  Talk = if Animal == cat  -> "meow";
+            Animal == beef -> "mooo";
+            Animal == dog  -> "bark";
+            Animal == tree -> "bark";
+            true -> "fgdadfgna"
+         end,
+  {Animal, "says " ++ Talk ++ "!"}.
